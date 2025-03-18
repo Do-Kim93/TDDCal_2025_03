1d929331badd9418960798e83e07e82641d91d0b
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-18 15:38:18 +0900

    TDD에서 블루 버전

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -4,18 +4,9 @@ public class Calc {
 
     public static int run(String exp) {
         exp = exp.replace("- ", "+ -");
-        System.out.println(exp);
 
-        boolean needToPlus = exp.contains("+");
-        boolean needToMinus = exp.contains("-");
 
-        String[] bits = null;
-
-        if (needToPlus) {
-            bits = exp.split(" \\+ ");
-        } else if (needToMinus) {
-            bits = exp.split(" - ");
-        }
+        String[] bits = exp.split(" \\+ ");
 
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
@@ -26,13 +17,9 @@ public class Calc {
         }
 
 
-        if (needToPlus) {
-            return a + b + c;
-        } else if (needToMinus) {
-            return a - b;
-        }
+        return a + b + c;
 
-        throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
+//        throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
     }
 
 }
