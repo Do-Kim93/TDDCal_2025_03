6e21bb4a37346317d1c9d6c47d3b5f28b48207f5
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-20 09:02:22 +0900

    ((20 + 20)) + 20 == 60

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,49 +1,93 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
-//10 + 5 * 2 문제는 곱하기를 먼저 해야한다 그리고 더하기
-//먼저 곱하고 그 곱한 값을 지역 변수에 저장해 놨다가 더하기 연사자랑 연산하기
 public class Calc {
-    public static int run(String exp) {
-        boolean dd = exp.contains("*");
-        exp = exp.replace("- ", "+ -");
-        String[] bits;
-        if (dd) {
-            bits = exp.split(" \\* ");
-            System.out.println(Arrays.toString(bits));
-            int[] nums = new int[bits.length];
-            int sum = 1;
-            for (int i = 0; i < bits.length; i++) {
-                String l = bits[i];
-                try {
-                    nums[i] = Integer.parseInt(bits[i]);
-                    sum *= nums[i];
-                    System.out.println(sum);
-                }catch(NumberFormatException e){
-//                    boolean ab = l.contains("+");
-                    bits = exp.split(" \\+ ");
-                    int[] nums1 = new int[bits.length];
-                    for (int k = 0; k < bits.length; k++) {
-                        nums1[k] = Integer.parseInt(bits[k]);
-                        sum += nums1[k];
-                    }
-            }
-            }
-            return sum;
-        } else {
-            bits = exp.split(" \\+ ");
-            int[] nums = new int[bits.length];
-            int sum = 0;
-            for (int i = 0; i < bits.length; i++) {
-                nums[i] = Integer.parseInt(bits[i]);
-                sum += nums[i];
-            }
-            return sum;
 
+    public static int run(String exp) {
+        // 괄호 제거
+        exp = stripOuterBrackets(exp);
+
+        // 단일항이 들어오면 바로 리턴
+        if (!exp.contains(" ")) {
+            return Integer.parseInt(exp);
         }
 
-//       throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
-    }
-}
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
+        boolean needToSplit = exp.contains("(") || exp.contains(")");
+        boolean needToCompound = needToPlus && needToMulti;
 
+        if (needToSplit) {
+            int bracketsCount = 0;
+            int splitPointIndex = -1;
+
+            for (int i = 0; i < exp.length(); i++) {
+                if (exp.charAt(i) == '(') {
+                    bracketsCount++;
+                } else if (exp.charAt(i) == ')') {
+                    bracketsCount--;
+                }
+                if (bracketsCount == 0) {
+                    splitPointIndex = i;
+                    break;
+                }
+            }
+            String firstExp = exp.substring(0, splitPointIndex + 1);
+            String secondExp = exp.substring(splitPointIndex + 4);
+
+            return Calc.run(firstExp) + Calc.run(secondExp);
+
+        } else if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return run(newExp);
+        }
+
+        if (needToPlus) {
+            exp = exp.replace("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아닙니다");
+    }
+
+    private static String stripOuterBrackets(String exp) {
+
+        int outerBracketsCount = 0;
+
+        while (exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')') {
+            outerBracketsCount++;
+        }
+
+        if (outerBracketsCount == 0) return exp;
+
+        return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
+    }
+
+}
